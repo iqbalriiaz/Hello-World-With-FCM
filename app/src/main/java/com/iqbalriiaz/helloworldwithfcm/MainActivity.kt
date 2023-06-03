ba5457e28d738075402e2a55c44704b051845849
iqbalriiaz <iqbalriiaz@gmail.com>
2023-06-03 18:32:58 +0600

    added view binding, two buttons and one textview

--- app/src/main/java/com/iqbalriiaz/helloworldwithfcm/MainActivity.kt
+++ app/src/main/java/com/iqbalriiaz/helloworldwithfcm/MainActivity.kt
@@ -2,10 +2,18 @@ package com.iqbalriiaz.helloworldwithfcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iqbalriiaz.helloworldwithfcm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnChangeText.setOnClickListener {
+            binding.dynamicTextView.setText(R.string.changed_text)
+        }
     }
 }
