02cce3d9aced724b698e1f38e9d9d8f045ec5295
iqbalriiaz <iqbalriiaz@gmail.com>
2023-06-04 01:40:17 +0600

    added fcm, notificationManager, connectivityManager and toast

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Hello-World-With-FCM
-A simple hello world task with Firebase Cloud Messaging
+# Hello-World-with-FCM
+
+This is a basic Android app built using Kotlin that allows the user to change the text in a text field by clicking a button and receive push notifications using Firebase Cloud Messaging (FCM).
+
+## Functionality
+
+The app includes the following features:
+
+1.  User Interface: The app has a simple user interface with two buttons and a text field.
+
+2.  Button Functionality:
+    * When one button is clicked, it changes the text in the text field to "Hello, World!"
+    *  When another one is clicked, it triggers a push notification to the device.
+
+3.  Push Notifications: The app implements FCM to receive push notifications.
+
+
+## Setup and Running the Project
+
+To run the project, please follow these steps:
+
+1.  Clone the repository from the provided Git link: [repository-link](https://github.com/iqbalriiaz/Hello-World-With-FCM).
+
+2.  Open the project in Android Studio.
+
+3.  Build and run the app on an Android emulator or physical device.
+
+4.  Click the button to change the text in the text field to "Hello, World!"
+
+5.  Click the push notification button to trigger a test push notification.
+
+
+Note: Make sure you have set up Firebase Cloud Messaging and have the necessary configuration files in your project for push notifications to work correctly.
+
+## UI
+
--- app/src/main/java/com/iqbalriiaz/helloworldwithfcm/MainActivity.kt
+++ app/src/main/java/com/iqbalriiaz/helloworldwithfcm/MainActivity.kt
@@ -1,19 +1,131 @@
 package com.iqbalriiaz.helloworldwithfcm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessaging
 import com.iqbalriiaz.helloworldwithfcm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var notificationManager: NotificationManagerCompat
+
+    // Set up the activity
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set up click listener for changing Text View
         binding.btnChangeText.setOnClickListener {
             binding.dynamicTextView.setText(R.string.changed_text)
         }
+
+        // Set up click listener for triggering push notification
+        binding.btnSendNotification.setOnClickListener {
+            if (isInternetConnected()) {
+                subscribeToTopic()
+                Log.d("MainActivity", "btnSendNotification clicked")
+            } else {
+                "No internet connection".showToast()
+            }
+        }
+
+        // Create notification manager
+        notificationManager = NotificationManagerCompat.from(this)
+
+        // Create notification channel for Android 8.0 and above
+        createNotificationChannel()
+    }
+
+    // Subscribe to a Firebase Messaging topic
+    private fun subscribeToTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic("test_topic")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("MainActivity", "Subscribed to topic")
+                    sendTestPushNotification()
+                } else {
+                    Log.e("MainActivity", "Failed to subscribe to topic", task.exception)
+                }
+            }
+    }
+
+    // Send a test push notification
+    private fun sendTestPushNotification() {
+        // Create notification builder
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Test Notification")
+            .setContentText("This is a test push notification")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        // Show the notification
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
+    // Create a notification channel for Android 8.0 and above
+    private fun createNotificationChannel() {
+        // Check if the device is running Android 8.0 or above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Channel"
+            val description = "Channel for test notifications"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                this.description = description
+            }
+
+            // Register the notification channel with the system
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    // Check if the device is connected to the internet
+    @SuppressLint("NewApi")
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun isInternetConnected(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val activeNetwork =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        return activeNetwork.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
+    // Extension function to show a Toast message
+    private fun String.showToast() {
+        Toast.makeText(this@MainActivity, this, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "test_channel"
+        private const val NOTIFICATION_ID = 1
     }
 }
